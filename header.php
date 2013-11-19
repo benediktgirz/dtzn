3c4369544a4f6e0f65b2f5ff38cf1a675296f94c
danbender <=>
2013-11-18 19:38:37 -0800

    first commit used wget to scrape html, then adding php includes. install wget with `brew install wget`

---
+++ header.php
@@ -0,0 +1,80 @@
+            <header class="header">
+            <div class="container">
+
+        <span class="row-fluid">
+            <span class="span7">
+
+                <!-- Logo -->
+                <div id="logo" class="clearfix">
+
+
+                        <a href="http://dtzn.de">
+                            <img src="http://dtzn.de/wp-content/uploads/2013/03/Logo-DTZN-e1365338197752.gif" alt="Diagnose Therapie Zentrum Nordenstadt" />
+                        </a>
+
+
+
+                </div>
+
+            </span>
+
+                        <span class="span5">
+
+                <!-- Contact Info -->
+                <ul class="contact">
+
+                                            <li>
+                            <a href="mailto:praxis@dtzn.de"><i class="icon-envelope icon-white"></i>
+                                <div itemtype="http://schema.org/MedicalOrganization" itemscope>
+                                    <div itemprop="email" content="praxis@dtzn.de">
+                                        praxis@dtzn.de                                    </div>
+                                </div>
+                            </a>
+                        </li>
+
+                                            <li>
+                            <span>
+                                <i class="icon-phone icon-white"></i>
+                                    <div itemtype="http://schema.org/MedicalOrganization" itemscope>
+                                        <div itemprop="telephone" content="0612213016 & 0612213017">
+                                            0612213016 & 0612213017                                        </div>
+                                    </div>
+                            </span>
+                        </li>
+                                    </ul>
+
+
+                    <div class="widgets">
+                        <div class="widget">            <div class="textwidget"><h4>Dr. med. Michaela Wende</h4> <p>Allgemeinmedizin - Sportmedizin - Chirotherapie<br /> Schmerztherapie - Palliativmedizin - Hausärztin</p>
+<h4>Dr. med. Dimitrios Zgouras</h4>
+<p>Innere Medizin - Gefäßmedizin (Angiologie)<br /> Männergesundheit - Hausarzt</p></div>
+        </div>                    </div>
+
+            </span>
+        </span>
+
+        <span class="row-fluid">
+            <span class="span12">
+
+                <!-- Navigation -->
+                <div class="navbar">
+                    <div>
+                      <div>
+                        <a class="btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                          <i class="icon-list icon-white"></i>
+                        </a>
+                        <div class="nav-collapse">
+
+
+                                    <div class="menu clearfix"><ul><li class="current_page_item"><a href="http://dtzn.de/" title="Home">Home</a></li><li class="page_item page-item-48"><a href="http://dtzn.de/online-dienste/">Online Dienste</a></li><li class="page_item page-item-14"><a href="http://dtzn.de/praxis/">Praxis</a></li><li class="page_item page-item-16"><a href="http://dtzn.de/leistungen/">Leistungen</a><ul class='children'><li class="page_item page-item-18"><a href="http://dtzn.de/leistungen/check-up/">Check up</a></li><li class="page_item page-item-20"><a href="http://dtzn.de/leistungen/check-up-plus/">Check up plus</a></li></ul></li><li class="page_item page-item-30"><a href="http://dtzn.de/team/">Team</a><ul class='children'><li class="page_item page-item-266"><a href="http://dtzn.de/team/dr-med-m-wende/">Dr. med. Michaela Wende</a></li><li class="page_item page-item-262"><a href="http://dtzn.de/team/dr-med-d-zgouras/">Dr. med. Dimitrios Zgouras</a></li></ul></li><li class="page_item page-item-26"><a href="http://dtzn.de/neuigkeiten/">Neuigkeiten</a></li><li class="page_item page-item-24"><a href="http://dtzn.de/karriere/">Karriere</a></li><li class="page_item page-item-54"><a href="http://dtzn.de/kontakt/">Kontakt</a><ul class='children'><li class="page_item page-item-270"><a href="http://dtzn.de/kontakt/impressum/">Impressum</a></li></ul></li><li class="page_item page-item-207"><a href="http://dtzn.de/anfahrt/">Anfahrt</a></li></ul></div>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+            </span>
+        </span>
+
+
+        </div>
+        </header>
